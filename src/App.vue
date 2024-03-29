86d901c6b5022ddd3c720bbfbf12ef26c81cc24d
biranastasiia <biryukstasiya@gmail.com>
2024-03-29 10:30:53 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="app-logo" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +17,24 @@ export default {
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+}
+html, body {
+    height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 100%;
+    height: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: rgb(126, 234, 183);
+}
+.app-logo {
+    margin-top: 60px;
 }
 </style>
